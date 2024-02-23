b51c3cd790d725a79a01b1b5e9af292e856365fb
Ty Butler <butlerty@sheridancollege.ca>
2024-02-23 16:03:19 -0500

    added design class

---
+++ src/midtermexam_versiona_extensioncode/ty_Design.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+
+/**
+ *
+ * @author tybutler
+ */
+public class ty_Design {
+    //my favourite subject is java!
+    
+}
